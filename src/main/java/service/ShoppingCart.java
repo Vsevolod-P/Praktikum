cb2c95e0fd735eef0db789a0d88794bdd6653e7b
Vsevolod Petrenko <vpetrenko@ivi.ru>
2024-10-18 16:05:54 +0300

    Merge remote-tracking branch 'origin/final' into final
    # Conflicts:
    #	src/main/java/service/ShoppingCart.java

--- src/main/java/service/ShoppingCart.java
+++ src/main/java/service/ShoppingCart.java
@@ -1,7 +1,41 @@
 package service;
-
+import model.Discountable;
 import model.Food;
 
 public class ShoppingCart {
-    Food[] foods = new Food[99];
+    private Food[] items;
+
+    public ShoppingCart(Food[] items) {
+        this.items = items;
+    }
+
+    public double getTotalPriceWithoutDiscount() {
+        double total = 0;
+        for (Food item : items) {
+            total = total + item.getTotalPrice();
+        }
+        return total;
+    }
+     public double getTotalPriceWithDiscount() {
+        double total = 0;
+        for (Food item : items) {
+            if (item.getDiscount() != 0) {
+                total = total + item.getTotalPrice() *  (1 - ((Discountable) item).getDiscount()/100);
+            }  else {
+                total = total + item.getTotalPrice();
+            }
+        }
+        return total;
+    }
+
+    public double getTotalVegetarianPriceWithoutDiscount() {
+        double total = 0;
+        for (Food item : items) {
+            if (item.isVegetarian()) {
+                total = total + item.getTotalPrice();
+            }
+        }
+        return total;
+    }
+
 }
